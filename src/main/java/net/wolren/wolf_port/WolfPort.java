64e783abde7526fbc7f49032f4a9afb5c5b52308
Wolren <86470291+Wolren@users.noreply.github.com>
2024-03-10 19:59:28 +0100

    1.19.4

---
+++ src/main/java/net/wolren/wolf_port/WolfPort.java
@@ -0,0 +1,15 @@
+package net.wolren.wolf_port;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WolfPort implements ModInitializer {
+    public static final String MOD_ID = "wolf_port";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
