fccdbcc25c0fbaee93d1b6e02fc0b41a40250906
Wolren <86470291+Wolren@users.noreply.github.com>
2024-03-10 16:55:05 +0100

    #3

--- src/main/java/net/wolren/wolf_port/WolfPort.java
+++ src/main/java/net/wolren/wolf_port/WolfPort.java
@@ -1,12 +1,6 @@
 package net.wolren.wolf_port;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.WolfEntity;
-import net.wolren.wolf_port.entity.ModEntities;
-import net.wolren.wolf_port.entity.NewWolfEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,13 +10,6 @@ public class WolfPort implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ModEntities.registerEntities();
-        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
-            if (entity instanceof WolfEntity && !(entity instanceof NewWolfEntity)) {
-                entity.remove(Entity.RemovalReason.DISCARDED);
-            }
-        });
 
-        FabricDefaultAttributeRegistry.register(ModEntities.NEW_WOLF, NewWolfEntity.createWolfAttributes());
     }
 }
